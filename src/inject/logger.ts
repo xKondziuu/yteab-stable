a414cb936882355bc5b84db29bedc956b34b889c
KondRoll <kondroll.210@outlook.com>
2023-11-01 18:02:06 +0100

    Added functions log, error and warn

--- src/inject/logger.ts
+++ src/inject/logger.ts
@@ -1,5 +1,8 @@
 import inject from '.'
+import { name } from '../../package.json'
 
+
+let prefix = name.toLocaleUpperCase()
 
 /**
  * Moduł z funkcjami do tworzenia logów w konsoli przeglądarki użytkownika.
@@ -8,6 +11,16 @@ import inject from '.'
  */
 export const logger: inject.Logger = {
 
+  log(message:string) {
+    console.log(`[${prefix}] - ${String(message)}`)
+  },
 
+  error(message:string) {
+    console.error(`[${prefix}] - ${String(message)}`)
+  },
+
+  warn(message:string) {
+    console.warn(`[${prefix}] - ${String(message)}`)
+  }
 
 }
