8acd48344ebfff881197acea47db9e5f1af654e0
KondRoll <kondroll.210@outlook.com>
2023-11-01 08:24:37 +0100

    Added interfaces for inject scripts

--- src/typings/inject.d.ts
+++ src/typings/inject.d.ts
@@ -1,8 +1,42 @@
+/**
+ * Deklaracje typów dla inject.
+ * @namespace inject
+ * @see /src/inject/index.ts
+ */
+
 declare namespace inject {
 
   interface Index {
 
     _v: string
+
+  }
+
+  interface Events {
+
+
+
+  }
+
+  interface Logger {
+
+
+
+  }
+
+  namespace watch {
+
+    interface Embed {
+
+
+
+    }
+
+    interface Sync {
+
+
+
+    }
 
   }
 
