bf9e5291f81bf6d090204b5797b7e7c3f65238ab
KondRoll <kondroll.210@outlook.com>
2023-11-05 22:10:20 +0100

    Added all iframe parameters

--- src/typings/youtube.d.ts
+++ src/typings/youtube.d.ts
@@ -14,10 +14,22 @@ declare namespace YouTube {
 
       interface settings {
         autoplay: 0|1
+        cc_load_policy: 1
+        color: 'red'|'white'
+        controls: 0|1|2
+        disablekb: 0|1
         enablejsapi: 0|1
+        end: number
         fs: 0|1
+        hl: string
+        iv_load_policy: 1|3
+        list: 'string'
+        listType: 'playlist'|'search'|'user_uploads'
+        loop: 0|1
         modestbranding: 0|1
         origin: string
+        playlist: string
+        playsinline: 0|1
         rel: 0|1
         showinfo: 0|1
         start: number
