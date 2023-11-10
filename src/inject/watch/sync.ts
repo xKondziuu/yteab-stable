ca086ecc5886156c7e04d4ed0ebb6dda8dc8ca25
KondRoll <kondroll.210@outlook.com>
2023-11-10 17:08:48 +0100

    Added comments to functions

--- src/inject/watch/sync.ts
+++ src/inject/watch/sync.ts
@@ -11,14 +11,28 @@ import { ytelem, ytif } from '../../main'
  */
 export const sync: inject.watch.sync.Module = {
 
-  safestart(yt_navigate_finish:YouTube.EventResponse.Event.yt_navigate_finish, rate?:number) {
+  /**
+   * Funkcja uruchamiająca funkcję synchronizacji startnow(rate) w sposób bezpieczny, dzięki dodatkowemu pozyskaniu odpowiedzi
+   * z eventu yt-navigate-finish sprawdzamy wiele rzeczy m.in. długość, obecność wideo i po ich potwierdzeniu kontynuujemy.
+   * @param {Event|unknown} yt_navigate_finish - Objekt event zwracany przez addEventListener po wywołaniu eventu yt-navigate-finish
+   * @param {number} rate - Wartość (w sekundach) określająca delay interwału synchronizacji wideo, domyślna wartość 4 sekundy
+   */
+  safestart(yt_navigate_finish:YouTube.EventResponse.Event.yt_navigate_finish, rate:number = 4) {
 
   },
 
+  /**
+   * Funkcja uruchamiająca synchronizację wideo w embed do tego na stronie natychmiast po wywołaniu funkcji, interwał jest
+   * tworzony i uruchamiany od razu bez względu na obecnie wyświetlaną zawartość, długość wideo i jego obecność na stronie.
+   * @param {number} rate - Wartość (w sekundach) określająca delay interwału synchronizacji wideo, domyślna wartość 4 sekundy
+   */
   startnow(rate:number) {
 
   },
 
+  /**
+   * Funkcja zatrzymująca interwał synchronizacji natychmiast po wywołaniu bez względu na to czy jest on utworzony.
+   */
   stop() {
 
   }
